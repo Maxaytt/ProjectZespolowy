cc36865a2cb7482d46cb3151aa39f9b1bc40357b
Piskunov <nikolajpiskunov14@gmail.com>
2024-10-08 18:42:01 +0200

    refactor: add models folder

--- Domain/User.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Domain;
-
-public class User : IdentityUser<Guid>
-{
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-}
