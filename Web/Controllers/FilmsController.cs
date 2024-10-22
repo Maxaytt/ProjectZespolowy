434abe68750f91e9e88c28c80074d76a96976f0f
Николай Пискунов <nikolajpiskunov14@gmail.com>
2024-10-22 19:19:21 +0200

    feat: add Films controller

--- Web/Controllers/FilmsController.cs
+++ Web/Controllers/FilmsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Domain.Models;
 using Domain.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using Domain.ViewModel;
 
 namespace Web.Controllers;
 
@@ -31,6 +32,79 @@ public class FilmsController : Controller
     public IActionResult Create()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateEditFilmVm film)
+    {
+        var imageForDatabse = new Image
+        {
+            Id = Guid.NewGuid(),
+            Caption = film.Name,
+            ContentType = film.ImageFile.ContentType,
+        };
+        var filmForDatabase = new Film
+        {
+            Id = Guid.NewGuid(),
+            Name = film.Name,
+            Image = imageForDatabse,
+            ContentType = film.VideoFile.ContentType
+        };
+
+        if (film.ImageFile is not null)
+        {
+            using var imageStream = new MemoryStream();
+            film.ImageFile.CopyTo(imageStream);
+            imageForDatabse.Content = imageStream.ToArray();
+        }
+
+        if (film.VideoFile is not null)
+        {
+            using var filmStream = new MemoryStream();
+            film.VideoFile.CopyTo(filmStream);
+            filmForDatabase.Content = filmStream.ToArray();
+        }
+
+        _dbContext.Images.Add(imageForDatabse);
+        _dbContext.Films.Add(filmForDatabase);
+        _dbContext.SaveChanges();
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet("GetFilmAsResource/{id:guid}")]
+    public IActionResult GetFilmAsResource(Guid id)
+    {
+        var film = _dbContext.Films.Find(id);
+        if (film is null)
+            return NotFound();
+        return File(film.Content, film.ContentType, film.Name);
+    }
+
+    [HttpGet("GetImageAsResource/{id:guid}")]
+    public IActionResult GetImageAsResource(Guid id)
+    {
+        var image = _dbContext.Images.Find(id);
+        if (image is null)
+            return NotFound();
+        return File(image.Content, image.ContentType, image.Caption);
+    }
+
+    [HttpGet("PlayFilm/{id:guid}")]
+    public IActionResult PlayFilm(Guid id)
+    {
+        var film = _dbContext.Films.Find(id);
+
+        if (film == null)
+            return NotFound();
+
+        var viewModel = new PlayFilmVm()
+        {
+            Id = id,
+            Name = film.Name,
+            ContentType = film.ContentType,
+        };
+        return View(viewModel);
     }
 }
 
