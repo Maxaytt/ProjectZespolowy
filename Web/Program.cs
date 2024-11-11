43f2197ee8297344885ac4448795ceb325e840ca
MiraslauPiatselchyts <mirosslav11@gmail.com>
2024-11-11 18:04:31 +0100

    chore: configure DbContext for question handling in Program.cs

--- Web/Program.cs
+++ Web/Program.cs
@@ -6,6 +6,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddInfrastructure(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
@@ -29,5 +33,4 @@ app.MapControllerRoute(
     pattern: "{controller=Auth}/{action=Login}/{id?}");
 
 app.Run();
-
 public abstract partial class Program;
