238407016a9c377e8756e0b05566d84da9a72aa1
vivian <vivian.ectorius@gmail.com>
2024-11-13 12:25:52 +0100

    fix: edit film test using guid

--- IntegrationTests/Tests/AuthTests.cs
+++ IntegrationTests/Tests/AuthTests.cs
@@ -66,38 +66,6 @@ public class AuthTests : IDisposable
         Driver.Url.ShouldBe("http://localhost:5000/Home/Index");
     }
     
-    [Fact, Priority(2)]
-    public void Can_Edit_Film()
-    {
-        // Arrange
-        const string email = "testuser@example.com";
-        const string password = "Qwer1234!";
-        const string initialName = "Test";
-        const string updatedName = "UpdateName";
-
-        // Act
-        Driver.Navigate().GoToUrl("http://localhost:5000/");
-    
-        Driver.FindElement(By.Id("Email")).SendKeys(email);
-        Driver.FindElement(By.Id("Password")).SendKeys(password);
-        Driver.FindElement(By.CssSelector("button[type='submit']")).Click();
-
-        // Update the name to 'Test' and submit again
-        Driver.Navigate().GoToUrl("http://localhost:5000/Films/Edit/52f059d1-e00d-467c-a0f3-7d1adbdd9984");
-        Driver.FindElement(By.Id("Name")).Clear();
-        Driver.FindElement(By.Id("Name")).SendKeys(updatedName);
-        Driver.FindElement(By.CssSelector("button[type='submit']")).Click();
-
-        // Re-navigate to verify the final update
-        Driver.Navigate().GoToUrl("http://localhost:5000/Films/Edit/52f059d1-e00d-467c-a0f3-7d1adbdd9984");
-        Assert.Equal(updatedName, Driver.FindElement(By.Id("Name")).GetAttribute("value"));
-        
-        Driver.Navigate().GoToUrl("http://localhost:5000/Films/Edit/52f059d1-e00d-467c-a0f3-7d1adbdd9984");
-        Driver.FindElement(By.Id("Name")).Clear();
-        Driver.FindElement(By.Id("Name")).SendKeys(initialName);
-        Driver.FindElement(By.CssSelector("button[type='submit']")).Click();
-    }
-
     public void Dispose()
     {
         Driver.Quit();
---
+++ IntegrationTests/Tests/FilmTests.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using Shouldly;
+using Xunit.Priority;
+
+namespace IntegrationTests.Tests;
+
+public class FilmTests : IDisposable
+{
+    public readonly IWebDriver Driver = new EdgeDriver();
+    
+    [Fact, Priority(0)]
+    public void Should_UpdateFilm_When_Edit()
+    {
+        // Arrange
+        const string email = "testuser@example.com";
+        const string password = "Qwer1234!";
+        var uniqueName = Guid.NewGuid().ToString();
+
+        // Act 
+        Driver.Navigate().GoToUrl("http://localhost:5000/");
+        Driver.FindElement(By.Id("Email")).SendKeys(email);
+        Driver.FindElement(By.Id("Password")).SendKeys(password);
+        Driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+        
+        Driver.Navigate().GoToUrl("http://localhost:5000/Home/Index");
+        var firstFilmEditLink = Driver.FindElement(By.CssSelector("a.btn.btn-primary[href*='/Films/Edit']"));
+        firstFilmEditLink.Click();
+        
+        var nameField = Driver.FindElement(By.Id("Name"));
+        var oldName = nameField.GetAttribute("value");
+        nameField.Clear();
+        nameField.SendKeys(uniqueName);
+        Driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+
+        // Assert
+        Driver.Navigate().GoToUrl("http://localhost:5000/Home/Index");
+        var updatedFilm = Driver.FindElements(By.XPath($"//*[text()='{uniqueName}']"));
+        Assert.True(updatedFilm.Count > 0, "Updated film with unique name was not found.");
+        
+        firstFilmEditLink = Driver.FindElement(By.CssSelector("a.btn.btn-primary[href*='/Films/Edit']"));
+        firstFilmEditLink.Click();
+        nameField = Driver.FindElement(By.Id("Name"));
+        nameField.Clear();
+        nameField.SendKeys(oldName);
+        Driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+    }
+
+    public void Dispose()
+    {
+        Driver.Quit();
+        Driver.Dispose();
+    }
+}
