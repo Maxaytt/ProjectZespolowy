37fb8b628d2c9af3c350312a42ef9bf31a254a8f
Maxaytt <maxaytt@gmail.com>
2024-11-14 11:20:08 +0100

    fix: add auth test priority

--- IntegrationTests/Tests/AuthTests.cs
+++ IntegrationTests/Tests/AuthTests.cs
@@ -6,6 +6,7 @@ using Xunit.Priority;
 
 namespace IntegrationTests.Tests;
 
+[TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
 public class AuthTests : IDisposable
 {
     public readonly IWebDriver Driver = new EdgeDriver();
