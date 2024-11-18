8797487267b9b2bff87c464a6802071dabfc9e02
Maxaytt <maxaytt@gmail.com>
2024-11-18 11:12:51 +0100

    fix: remove redundant db connection

--- Web/Program.cs
+++ Web/Program.cs
@@ -7,9 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddInfrastructure(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
