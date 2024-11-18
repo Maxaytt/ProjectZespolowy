c1413c6abf03f44f6d830cea88c5cc062989664c
Mykola Piskunov <nikolajpiskunov14@gmail.com>
2024-11-18 18:16:40 +0100

    fix: change question model

--- Domain/Models/Quiestion.cs
+++ Domain/Models/Quiestion.cs
@@ -1,10 +1,11 @@
-﻿namespace Domain.Models;
-
-public class Question
+﻿namespace Domain.Models
 {
-    public Guid Id { get; set; }
-    public string Text { get; set; } = null!;
-    public Guid FilmId { get; set; }
-    public Film Film { get; set; } = null!;
-    public ICollection<Answer> Answers { get; set; } = null!;
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; } = null!;
+        public Guid FilmId { get; set; }
+        public Film Film { get; set; } = null!;
+        public ICollection<Answer> Answers { get; set; } = new List<Answer>(); 
+    }
 }
