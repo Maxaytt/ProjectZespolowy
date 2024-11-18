2398d46867ab976d02af6acbddb66b9115f1597d
Maxaytt <maxaytt@gmail.com>
2024-11-18 14:46:01 +0100

    feat: add ids and classes for tests

--- IntegrationTests/Tests/FilmsTests.cs
+++ IntegrationTests/Tests/FilmsTests.cs
@@ -1,7 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
 using Shouldly;
-using Xunit;
 using Xunit.Priority;
 
 namespace IntegrationTests.Tests
@@ -13,17 +12,10 @@ namespace IntegrationTests.Tests
         private const string LoginUrl = "http://localhost:5000/Auth/Login";
         private const string Email = "testuser@example.com";
         private const string Password = "Qwer1234!";
-        private static string? _filmName; 
 
         public FilmsTests()
         {
             _driver = new EdgeDriver();
-        }
-
-        public void Dispose()
-        {
-            _driver.Quit();
-            _driver.Dispose();
         }
 
         private void Login()
@@ -33,8 +25,6 @@ namespace IntegrationTests.Tests
             _driver.FindElement(By.Id("Email")).SendKeys(Email);
             _driver.FindElement(By.Id("Password")).SendKeys(Password);
             _driver.FindElement(By.CssSelector("button[type='submit']")).Click();
-
-            _driver.Url.ShouldBe(BaseUrl);
         }
 
         [Fact, Priority(0)]
@@ -42,21 +32,29 @@ namespace IntegrationTests.Tests
         {
             // Arrange
             Login();
-            _driver.Navigate().GoToUrl($"{BaseUrl}/Films");
 
             // Act
-            var filmRow = _driver.FindElements(By.CssSelector("tr.film-row")).FirstOrDefault();
+            var filmRow = _driver.FindElements(By.CssSelector(".film-item")).FirstOrDefault();
             filmRow.ShouldNotBeNull("No films found to delete.");
 
-            _filmName = filmRow.FindElement(By.CssSelector(".film-name")).Text;
+            var filmName = filmRow.FindElement(By.CssSelector(".card .film-name")).Text;
 
-            var deleteButton = filmRow.FindElement(By.CssSelector(".btn-delete"));
+            var deleteButton = filmRow.FindElement(By.CssSelector("#delete-btn"));
             deleteButton.Click();
 
             // Assert
-            var filmsList = _driver.FindElements(By.CssSelector("tr.film-row"));
-            filmsList.ShouldNotContain(f => f.FindElement(By.CssSelector(".film-name")).Text == _filmName,
-                "The film was not removed from the list.");
+            var filmsList = _driver.FindElements(By.CssSelector("#film-list"));
+            var isFilmDeleted = filmsList.All(element => 
+                element.FindElements(By.CssSelector(".film-name")).Count == 0 ||
+                element.FindElement(By.CssSelector(".film-name")).Text != filmName);
+
+            isFilmDeleted.ShouldBeTrue("The film was not removed from the list.");
+        }
+        
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
         }
     }
 }
