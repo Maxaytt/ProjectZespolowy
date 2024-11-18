ce029d2c3ce52d1c75a183018421179236b11209
Mykola Piskunov <nikolajpiskunov14@gmail.com>
2024-11-18 18:16:17 +0100

    feat: add AddAnswer view model

---
+++ Domain/ViewModel/AddAnswerVm.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.ViewModel
+{
+    public class AddAnswerVm
+    {
+        public Guid QuestionId { get; set; }
+        public string QuestionText { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public bool IsTrue { get; set; }
+
+        public List<AnswerVm> Answers { get; set; } = new();
+    }
+
+    public class AnswerVm
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; } = null!;
+        public bool IsTrue { get; set; }
+    }
+}
