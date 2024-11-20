fadd901e7b44db781209916daba8290d177388dd
Maxaytt <maxaytt@gmail.com>
2024-11-20 14:37:27 +0100

    refactor: refactor delete question test

--- IntegrationTests/Tests/QuestionTests.cs
+++ IntegrationTests/Tests/QuestionTests.cs
@@ -8,45 +8,40 @@ using Xunit;
 
 namespace IntegrationTests.Tests;
 
-public class DeleteQuestionTests : IDisposable
+public class QuestionTests : IDisposable
 {
     public readonly IWebDriver Driver = new EdgeDriver();
     private const string BaseUrl = "http://localhost:5000/";
+    private const string Email = "testuser@example.com";
+    private const string Password = "Qwer1234!";
 
     [Fact, Priority(0)]
     public void Should_DeleteFirstQuestion_When_DeleteQuestion()
     {
-        const string email = "testuser@example.com";
-        const string password = "Qwer1234!";
-
+        // Arrange
         Driver.Navigate().GoToUrl(BaseUrl);
-        Driver.FindElement(By.Id("Email")).SendKeys(email);
-        Driver.FindElement(By.Id("Password")).SendKeys(password);
+        Driver.FindElement(By.Id("Email")).SendKeys(Email);
+        Driver.FindElement(By.Id("Password")).SendKeys(Password);
         Driver.FindElement(By.CssSelector("button[type='submit']")).Click();
 
         var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
         wait.Until(d => d.Url == $"{BaseUrl}Home/Index");
 
+        // Act
         var editButton = Driver.FindElement(By.CssSelector("a.btn.btn-primary"));
         editButton.Click();
         wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("input[name='Name']")));
 
         var questionsBeforeDelete = Driver.FindElements(By.CssSelector(".rounded.p-3.mb-3.bg-light.d-flex.justify-content-between.align-items-center.border"));
-        int initialQuestionCount = questionsBeforeDelete.Count;
+        var initialQuestionCount = questionsBeforeDelete.Count;
         
-        if (initialQuestionCount == 0)
-        {
-            throw new NoSuchElementException("No questions for deletion.");
-        }
+       initialQuestionCount.ShouldNotBe(0);
         
         var deleteButton = questionsBeforeDelete[0].FindElement(By.CssSelector("a.btn.btn-danger"));
         deleteButton.Click();
-
-        Driver.Navigate().Refresh();
-
-        Console.WriteLine("Waiting for the refreshed 'Edit Film' page to load...");
+        
         wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("input[name='Name']")));
-
+        // Assert
         var updatedQuestions = Driver.FindElements(By.CssSelector(".rounded.p-3.mb-3.bg-light.d-flex.justify-content-between.align-items-center.border"));
         updatedQuestions.Count.ShouldBe(initialQuestionCount - 1); 
     }
